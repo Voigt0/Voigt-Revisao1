137900b7ca56007b7924eed49062c3b35118fdd5
Voigt0 <voigtrodrigo0@gmail.com>
2021-07-21 17:42:18 -0300

    Update index1.php
    Added nav bar

--- index1.php
+++ index1.php
@@ -6,8 +6,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Star Wars</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Episódio 1</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index1.php">Select</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index2.php">Radio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index3.php">Checkbox</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Radio</a>
+        </li>
+    </div>
+  </div>
+    </nav>
     <form method="post">
         <select name="nome" id="nome">
             <option value="1" <?php if ($nome == 1) echo "selected"; ?>>Luke Skywalker</option>
